c157eb2ae28364a1c90696958443dad36ca120a0
Eonvorax <mail4bishop@gmail.com>
2024-01-03 09:52:01 +0100

    Fix: missing type annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ This is the async_generator module
 
 import asyncio
 from random import uniform
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     The coroutine will loop 10 times, each time asynchronously wait
     1 second, then yield a random number between 0 and 10
