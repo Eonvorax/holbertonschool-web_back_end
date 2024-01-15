3848295dff43604f9f6ddbe9220c244dc08185c9
Eonvorax <mail4bishop@gmail.com>
2024-01-15 13:31:17 +0100

    Task 1: Block Scope

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
+}
