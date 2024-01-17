571c6e98133cac31bee217c9e0199735329be892
Eonvorax <mail4bishop@gmail.com>
2024-01-17 11:22:58 +0100

    Task 6: Inheritance

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Calling the constructor of the parent class
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    this._floors = newFloors;
+  }
+
+  // Overriding the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
