c0edd1936453a65719118a50fb6a05f00215e044
Eonvorax <mail4bishop@gmail.com>
2024-01-17 11:26:56 +0100

    Fix: typo

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -17,6 +17,6 @@ export default class SkyHighBuilding extends Building {
 
   // Overriding the evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
