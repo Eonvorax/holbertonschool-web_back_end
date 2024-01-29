151a0ceb5684d7d034c5380a3eb3c2bd9e1bee49
Eonvorax <mail4bishop@gmail.com>
2024-01-29 14:16:52 +0100

    Task 9: Map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  // Concise syntax without set(key, value)
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
